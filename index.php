34226d7e7170cd086f8c28df421822ee650f2d10
shina1 <oswasiu@gmail.com>
2020-03-26 12:01:17 +0100

    added the form

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SignUp|Form</title>
+    <link href="index.css">
+</head>
+<body>
+<main>
+            <div class="info">
+                    <h3>Create </h3> 
+                    <h3>Your Account</h3>
+            </div>  
+        <div class="login-form form-control">
+            <form action="" id="form">
+                <div id="input" class="input4"><input type="text" id="name" placeholder="Full Name" autofocus></div>
+                <div id="input" class="input1"><input type="text" id="number" placeholder="Phone Number" autofocus></div>
+                <div id="input" class="input2"><input type="email" id="email" placeholder="Email" autofocus></div>
+                <div id="input" class="input5"><input type="city " id="city" placeholder="City" autofocus></div>
+                <div id="input" class="input3"><input type="password" id="password" placeholder="Password" autofocus></div>
+
+                <div class="form-break">
+                    <p><a href="forgotaccount.html">Forgot Account?</a></p>
+                    <input type="submit" value="SIGN UP" id="signup">
+                </div>
+            </form>
+            <div class="line">
+                    <hr style="width:220px; height:0;"> <p>or</p> <hr style="width:220px; height:0;">
+            </div>
+            <div class="icons">
+                    <div class="fonts">
+                            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i> </a> 
+                            <a href="#"> <i class="fa fa-google" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i> </a>
+                    </div>
+                    <p>Allready have an account? <a href="index.html">Login</a></p>
+            </div>
+            
+        </div>
+    </main> 
+</body>
+</html>
